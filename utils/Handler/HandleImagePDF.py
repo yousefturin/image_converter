c7dd6d7c029e51266a9692ca8f75451b9de96ec8
yousefturin <yusefturin@gmail.com>
2023-09-03 13:41:11 +0300

    fixed synatx of files

--- utils/Handler/HandleImagePDF.py
+++ utils/Handler/HandleImagePDF.py
@@ -6,9 +6,11 @@ from utils.ImageSize import GetImageSize
 def ConvertPNGToPDF(OriginalPath, Path):
     '''Take an image from its path and 
     the new path to be saved in as PDF'''
+    
     ImageWidth, ImageHeight = GetImageSize(OriginalPath)
     c = canvas.Canvas(Path, pagesize=letter)
     c.drawImage(OriginalPath, 0, 0, width=ImageWidth, height=ImageHeight)
     c.showPage()
     c.save()
+    
     return
